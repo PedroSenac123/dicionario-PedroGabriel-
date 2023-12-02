ad9aff0cfa8514e2989b6a8843d4129ce91ab10f
PedroSenac123 <126249729+PedroSenac123@users.noreply.github.com>
2023-12-01 23:56:51 -0300

    Add files via upload

---
+++ trabalho-site dicionario/app.js
@@ -0,0 +1,254 @@
+const schedule = document.querySelector("#schedule")
+schedule.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalSchedule.style.visibility = 'visible'
+})
+fecharModalSchedule.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    fundoDesfocado.style.visibility = 'hidden'
+    modalSchedule.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioSchedule').addEventListener('click', function() {
+    var audio = document.getElementById('audioSchedule')
+    audio.play();
+})
+document.getElementById('imgTocarAudioAgendar').addEventListener('click', function() {
+    var audio = document.getElementById('audioAgendar')
+    audio.play();
+})
+
+
+
+const accrue = document.querySelector("#accrue")
+accrue.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalAccrue.style.visibility = 'visible'
+})
+fecharModalAccrue.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'hidden'
+    modalAccrue.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioAccrue').addEventListener('click', function() {
+    var audio = document.getElementById('audioAccrue')
+    audio.play();
+})
+document.getElementById('imgTocarAudioAcumular').addEventListener('click', function() {
+    var audio = document.getElementById('audioAcumular')
+    audio.play();
+})
+
+
+
+const pseudonym = document.querySelector("#pseudonym")
+pseudonym.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalPseudonym.style.visibility = 'visible'
+})
+fecharModalPseudonym.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    fundoDesfocado.style.visibility = 'hidden'
+    modalPseudonym.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioPseudonym').addEventListener('click', function() {
+    var audio = document.getElementById('audioPseudonym')
+    audio.play();
+})
+document.getElementById('imgTocarAudioPseudonimo').addEventListener('click', function() {
+    var audio = document.getElementById('audioPseudonimo')
+    audio.play();
+})
+
+
+
+const jubilantly = document.querySelector("#jubilantly")
+jubilantly.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalJubilantly.style.visibility = 'visible'
+})
+fecharModalJubilantly.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    fundoDesfocado.style.visibility = 'hidden'
+    modalJubilantly.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioJubilantly').addEventListener('click', function() {
+    var audio = document.getElementById('audioJubilantly')
+    audio.play();
+})
+document.getElementById('imgTocarAudioJubilosamente').addEventListener('click', function() {
+    var audio = document.getElementById('audioJubilosamente')
+    audio.play();
+})
+
+
+
+const yachtswoman = document.querySelector("#yachtswoman")
+yachtswoman.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalYachtswoman.style.visibility = 'visible'
+})
+fecharModalYachtswoman.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    fundoDesfocado.style.visibility = 'hidden'
+    modalYachtswoman.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioYachtswoman').addEventListener('click', function() {
+    var audio = document.getElementById('audioYachtswoman')
+    audio.play();
+})
+document.getElementById('imgTocarAudioVelejadora').addEventListener('click', function() {
+    var audio = document.getElementById('audioVelejadora')
+    audio.play();
+})
+
+
+
+
+const quandary = document.querySelector("#quandary")
+quandary.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalQuandary.style.visibility = 'visible'
+})
+fecharModalQuandary.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    fundoDesfocado.style.visibility = 'hidden'
+    modalQuandary.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioQuandary').addEventListener('click', function() {
+    var audio = document.getElementById('audioQuandary')
+    audio.play();
+})
+document.getElementById('imgTocarAudioDilema').addEventListener('click', function() {
+    var audio = document.getElementById('audioDilema')
+    audio.play();
+})
+
+
+const sycophant = document.querySelector("#sycophant")
+sycophant.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalSycophant.style.visibility = 'visible'
+})
+fecharModalSycophant.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    fundoDesfocado.style.visibility = 'hidden'
+    modalSycophant.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioSycophant').addEventListener('click', function() {
+    var audio = document.getElementById('audioSycophant')
+    audio.play();
+})
+document.getElementById('imgTocarAudioBajulador').addEventListener('click', function() {
+    var audio = document.getElementById('audioBajulador')
+    audio.play();
+})
+
+
+
+
+const abstention = document.querySelector("#abstention")
+abstention.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalAbstention.style.visibility = 'visible'
+})
+fecharModalAbstention.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    fundoDesfocado.style.visibility = 'hidden'
+    modalAbstention.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioAbstention').addEventListener('click', function() {
+    var audio = document.getElementById('audioAbstention')
+    audio.play();
+})
+document.getElementById('imgTocarAudioAbstencao').addEventListener('click', function() {
+    var audio = document.getElementById('audioAbstencao')
+    audio.play();
+})
+
+
+
+
+const ebullient = document.querySelector("#ebullient")
+ebullient.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalEbullient.style.visibility = 'visible'
+})
+fecharModalEbullient.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    fundoDesfocado.style.visibility = 'hidden'
+    modalEbullient.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioEbullient').addEventListener('click', function() {
+    var audio = document.getElementById('audioEbullient')
+    audio.play();
+})
+document.getElementById('imgTocarAudioEfervescente').addEventListener('click', function() {
+    var audio = document.getElementById('audioEfervescente')
+    audio.play();
+})
+
+
+
+const discombobulate = document.querySelector("#discombobulate")
+discombobulate.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    fundoDesfocado.style.visibility = 'visible'
+    modalDiscombobulate.style.visibility = 'visible'
+})
+fecharModalDiscombobulate.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    fundoDesfocado.style.visibility = 'hidden'
+    modalDiscombobulate.style.visibility = 'hidden'
+})
+document.getElementById('imgTocarAudioDiscombobulate').addEventListener('click', function() {
+    var audio = document.getElementById('audioDiscombobulate')
+    audio.play();
+})
+document.getElementById('imgTocarAudioDesconcertar').addEventListener('click', function() {
+    var audio = document.getElementById('audioDesconcertar')
+    audio.play();
+})
+
+
+const aparecerDialogo = document.querySelector("#aparecerDialogo")
+aparecerDialogo.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    seta.style.rotate = '180deg'
+    dialogo.style.display = 'flex'
+})
+dialogo.addEventListener('click', (e) =>{
+    e.preventDefault() 
+ 
+    seta.style.rotate = '0deg'
+    dialogo.style.display = 'none'
+})
